badcd7f0bf9cf944d36226f4c22b48c77e6193d1
broadsign-lekevoid <kevin.gagnon@broadsign.com>
2023-10-09 14:51:45 -0400

    Orbit load initial element

--- components/PortfolioPageComponents.vue
+++ components/PortfolioPageComponents.vue
@@ -2,7 +2,7 @@
 	<PortfolioMasonry :items="items" @open-picture-orbit="(imgID) => openOrbitToImg(imgID)" />
 	<Teleport to="body">
 		<Transition name="fade" mode="out-in">
-			<PortfolioOrbit v-if="showOrbit" :items="items" @close-orbit="closeOrbit" />
+			<PortfolioOrbit v-if="showOrbit" :items="items" :initial-element="initialOrbitElementID" @close-orbit="closeOrbit" />
 		</Transition>
 	</Teleport>
 </template>
@@ -10,10 +10,11 @@
 <script setup>
 const { items } = defineProps(["items"]);
 const showOrbit = ref(false);
+const initialOrbitElementID = ref("");
 
 function openOrbitToImg(imgID) {
-	console.log(imgID);
 	showOrbit.value = true;
+	initialOrbitElementID.value = imgID;
 }
 
 function closeOrbit() {
