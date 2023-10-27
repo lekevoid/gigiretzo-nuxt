0cbc083a5a7731a00c4067c42100985620f1ff77
broadsign-lekevoid <kevin.gagnon@broadsign.com>
2023-10-27 15:20:45 -0400

    Tweak image sizes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	devtools: { enabled: true },
+
 	app: {
 		head: {
 			charset: "utf-8",
 			viewport: "width=device-width, initial-scale=1",
 		},
 	},
-
-	/* app: {
-		pageTransition: { name: "page", mode: "out-in" },
-	}, */
-
-	/* alias: {
-		images: fileURLToPath(new URL("./assets/img", import.meta.url)),
-		styles: fileURLToPath(new URL("./assets/styles", import.meta.url)),
-	}, */
 
 	modules: ["@nuxtjs/i18n", "@nuxt/image", "@pinia/nuxt"],
 
@@ -51,6 +43,7 @@ export default defineNuxtConfig({
 		format: ["avif", "webp"],
 		screens: {
 			xs: 320,
+			sm: 600,
 			md: 900,
 			xl: 1400,
 		},
